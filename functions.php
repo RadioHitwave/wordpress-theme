09a48e1d63ace1d3af3e0eff0a0b98f64660c656
Jan Brodda <git@janbrodda.de>
2020-01-07 10:28:04 +0100

    update tracking code

--- functions.php
+++ functions.php
@@ -1,6 +1,37 @@
 <?php
 
 function hitwave_inject_analytics() { ?>
+<noscript><p><img src="https://analytics.radiohitwave.com/trck.html?idsite=1&rec=1" alt=""/></p></noscript>
+<script type="text/javascript">
+	const trackingBaseUrl = 'https://analytics.radiohitwave.com/trck';
+	const trackingPageId = 1;
+	const trackingValidation = new Image(1, 1);
+	trackingValidation.onload = function () {
+		const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+		g.src = trackingBaseUrl + '.js';
+		g.async = true;
+		g.defer = true;
+		s.parentNode.insertBefore(g, s);
+
+		function waitForTracker() {
+			if (typeof Piwik !== "undefined") {
+				const tracker = Piwik.getTracker(trackingBaseUrl + ".html", trackingPageId);
+				tracker.enableLinkTracking();
+				tracker.trackPageView();
+			} else {
+				setTimeout(waitForTracker, 100);
+			}
+		}
+
+		waitForTracker();
+	};
+	trackingValidation.onerror = function () {
+		const d = document, g = d.createElement('img'), s = d.getElementsByTagName('script')[0];
+		g.src = trackingBaseUrl + '.html?idsite=' + trackingPageId + '&rec=1';
+		s.parentNode.insertBefore(g, s);
+	};
+	trackingValidation.src = trackingBaseUrl + '.html?action_name=&idsite=' + trackingPageId + '&rec=0&send_image=1';
+</script>
 <?php }
 add_action('wp_footer', 'hitwave_inject_analytics', 99999);
 
